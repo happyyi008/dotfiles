2c4aead435738839524a3b72eef2c855aea5dede
Yi Chen <happyyi008@gmail.com>
2017-12-27 21:16:51 -0500

    dont symlink git

--- init_dotfile.sh
+++ init_dotfile.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-find . -type f | while read -r  f; do
+# create symlinks in $HOME for all files in dotfiles
+find . -type f | grep -v '/.git/' | while read -r  f; do
     echo "ln -s $HOME/dotfiles/${f:2} $HOME/${f:2}"
     ln -s $HOME/dotfiles/${f:2} $HOME/${f:2}
 done 
