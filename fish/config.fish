61658cd6763aa3c9ae8db68ef2d24e74005d3ff7
Yi Chen <happyyi008@gmail.com>
2018-10-28 16:37:53 -0400

    put fish abbr in config

--- fish/config.fish
+++ fish/config.fish
@@ -4,3 +4,24 @@ fish_clipboard_paste
 custom_vi
 
 eval (direnv hook fish)
+
+abbr la 'ls -a'
+abbr k kubectl
+abbr l ls
+abbr dc docker-compose
+abbr dk docker
+abbr gcom 'git commit'
+abbr gst 'git stash'
+abbr gs 'git status'
+abbr ga 'git add .'
+abbr gsh 'git show'
+abbr bb git_current_branch
+abbr gco 'git checkout'
+abbr gb 'git branch'
+abbr glg git_pretty_log
+abbr lll 'tree -L 3 -C | less -r'
+abbr kns kubens
+abbr gd 'git diff'
+abbr gds 'git diff --cached'
+abbr gpb 'git push origin (git_current_branch)'
+
